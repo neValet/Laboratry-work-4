7b4ed8f68cbfc39133f9036cee0845d7e7c49f55
Admin <kaganyaknazar@gmail.com>
2018-11-21 21:05:48 +0200

    I have changed names of variables and class to make code more understandable

--- main.cpp
+++ main.cpp
@@ -1,40 +1,35 @@
-//hello Zenoviy Evgenovich!
 #include <iostream>
 #include <string>
 #include "Header.h"
 using namespace std;
 
 int main(){
-	char a;
-	cin >> a;
-	insect insect1(a, 2.2, 3.6, 2, 4);
-	insect1.getname();
-	insect1.getvelocity();
-	insect1.getweight();
-	insect1.getlenght();
-	insect1.getwingSize();
+	char insectsName;
+	cin >> insectsName;
+	Insect insect1(insectsName, 2.2, 3.6, 2, 4);
+	insect1.getName();
+	insect1.getVelocity();
+	insect1.getWeight();
+	insect1.getLenght();
+	insect1.getWingSize();
 
-	cin >> a;
-	insect insect2(a, 3.1, 2, 5, 3);
-	insect2.getname();
-	insect2.getvelocity();
-	insect2.getweight();
-	insect2.getlenght();
-	insect2.getwingSize();
+	cin >> insectsName;
+	Insect insect2(insectsName, 3.1, 2, 5, 3);
+	insect2.getName();
+	insect2.getVelocity();
+	insect2.getWeight();
+	insect2.getLenght();
+	insect2.getWingSize();
 
-	cin >> a;
-	insect insect3(a, 4, 1.3, 4, 2);
-	insect3.getname();
-	insect3.getvelocity();
-	insect3.getweight();
-	insect3.getlenght();
-	insect3.getwingSize();
+	cin >> insectsName;
+	Insect insect3(insectsName, 4, 1.3, 4, 2);
+	insect3.getName();
+	insect3.getVelocity();
+	insect3.getWeight();
+	insect3.getLenght();
+	insect3.getWingSize();
 
 	system("pause");
 
 	return 0;
-}
-/* �������� ���� �������� ������ ������ ����:
-- �����
-- �������� (����� �� �������)
-- ���� (� ������) */
+};
